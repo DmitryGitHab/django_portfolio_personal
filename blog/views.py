80b5f13d74d1661a9917d8005b07bbe9ed988bff
Dmitry <babanov91@gmail.com>
2022-07-07 21:52:54 +0500

    add start pages, url

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Blog
 
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')[:10]
-    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
